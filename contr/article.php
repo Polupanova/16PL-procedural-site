a00f9d05dca423351e77d05028c662e20f656a97
Dāvis Vēveris <davis.veveris@gmail.com>
2013-06-14 17:39:45 +0300

    article mainīgais
    added article.php to contr

---
+++ contr/article.php
@@ -0,0 +1,11 @@
+<?php
+
+/**
+ * Created by JetBrains PhpStorm.
+ * User: DVeveris
+ * Date: 13.14.6
+ * Time: 17:30
+ * To change this template use File | Settings | File Templates.
+ */
+
+$view = 'article';
