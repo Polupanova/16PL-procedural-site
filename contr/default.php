1c8d61dc530cd5dfd1e0a87ae9e9c365aa9b7c6b
Dāvis Vēveris <davis.veveris@gmail.com>
2013-06-14 17:52:19 +0300

    default.php bugfix
    cleaned up default.php

--- contr/default.php
+++ contr/default.php
@@ -1,11 +1,4 @@
-<? php
-//$array1= array('links1','links2','links3','links4');
-
-$assoc= array(array('title' => 'links1', 'link' => 'links1.php' ),
-        array('title' => 'links2', 'link' => 'links2.php'),
-		array('title' => 'links3', 'link' => 'links3.php'),
-		array('title' => 'links4', 'link' => 'links4.php'));
-
+<?php
 $nav_links=array(
 	'link1'=>'#',
 	'link2'=>'#',
@@ -13,9 +6,3 @@ $nav_links=array(
 );
 
 $article = 'article';
-/*$fn = function($arr) use ($array1) {
-    return !in_array($arr['title'], $array1);
-};
-$result =array_filter($assoc, $fn);*/
-
-?>
